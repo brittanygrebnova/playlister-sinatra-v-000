fa818007531691e2fec3d547b04208a68f64110e
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-10-02 15:20:37 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,6 +24,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(name: params[:Artist_Name])
     #binding.pry
     @song.genre_ids = params[:genres]
+    binding.pry
     @song.save
     flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
